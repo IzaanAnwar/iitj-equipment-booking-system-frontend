0764bab2923d0611cf49e1e6cd9914f7b999979c
IzaanAnwar <mdizaan67@gmail.com>
2024-03-22 22:33:02 +0530

    chore: base url

--- src/utils/axios-instance.ts
+++ src/utils/axios-instance.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 export const api = axios.create({
-  baseURL: 'https://crdsi-backend.vercel.app',
+  baseURL: 'https://crdsi-backend.onrender.com',
   withCredentials: true,
 });
