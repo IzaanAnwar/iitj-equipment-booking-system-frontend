70773e71fd6a3411ee2e43430fd9b45e15646ece
IzaanAnwar <mdizaan67@gmail.com>
2024-04-14 13:01:46 +0530

    refactor: /api add

--- src/utils/axios-instance.ts
+++ src/utils/axios-instance.ts
@@ -1,8 +1,6 @@
 import axios from 'axios';
-import Cookies from 'js-cookie';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8000',
-
+  baseURL: 'http://172.31.41.149:8000/api/',
   withCredentials: true,
 });
