b0be7984d04ef0a3f61400981d54560193fd4239
IzaanAnwar <mdizaan67@gmail.com>
2024-04-16 12:21:30 +0530

    style: login page

--- src/app/(global)/page.tsx
+++ src/app/(global)/page.tsx
@@ -14,9 +14,11 @@ export default function Home() {
 
 async function Header() {
   return (
-    <div className="flex items-center justify-center gap-12 pt-12 text-white">
-      <Image src={'/logo1.gif'} alt="crdsi logo" width={100} height={100} className="hidden md:block" />
-      <div className="flex w-full items-center justify-center gap-2 text-xl font-semibold md:text-2xl lg:text-4xl">
+    <div className="flex w-full items-center justify-center gap-12  pt-12 text-white">
+      <div className="hidden w-fit  md:block">
+        <Image src={'/logo1.gif'} alt="crdsi logo" width={100} height={100} />
+      </div>
+      <div className="w-fit items-center justify-center gap-2 text-center text-xl font-semibold md:flex md:text-2xl lg:text-4xl">
         <h3 className="font-bold">CRDSI</h3>
         <p>Equipment Booking System</p>
       </div>
