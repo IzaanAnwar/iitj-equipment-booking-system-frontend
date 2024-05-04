3d06b74353d72a9d6cc510f72999acdc17c178d5
IzaanAnwar <mdizaan67@gmail.com>
2024-05-04 15:47:53 +0530

    refactor: api url

--- src/utils/axios-instance.ts
+++ src/utils/axios-instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const apiEndpoint = 'http://localhost:8000/api/';
+const apiEndpoint = 'http://crdsibookingportal.iitj.ac.in:8000/api';
 export const api = axios.create({
   baseURL: apiEndpoint,
 });
