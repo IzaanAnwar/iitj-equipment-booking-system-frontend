1b1743e173423d51f25268fa3f31b42e6da8d7ee
IzaanAnwar <mdizaan67@gmail.com>
2024-05-18 19:38:29 +0530

    wrap up

--- src/utils/axios-instance.ts
+++ src/utils/axios-instance.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-// const apiEndpoint = 'http://crdsibookingportal.iitj.ac.in:8000/api';
-const apiEndpoint = 'http://localhost:8000/api';
+const apiEndpoint = 'http://crdsibookingportal.iitj.ac.in:8000/api';
+// const apiEndpoint = 'http://localhost:8000/api';
 export const api = axios.create({
   baseURL: apiEndpoint,
 });
